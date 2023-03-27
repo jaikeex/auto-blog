d9cc6f71ec987fb2a21513d45d34d4a1de452b4c
Jakub Hrubý <hrubyy.jakub@gmail.com>
2023-03-28 00:57:47 +0200

    updated to use the 3.5-turbo openai model

--- src/pages/api/generate-post/index.ts
+++ src/pages/api/generate-post/index.ts
@@ -14,6 +14,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
   });
+
   const openAI = new OpenAIApi(config);
 
   const topic = req.body.topic;
@@ -34,21 +35,30 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const response = await openAI.createCompletion({
-    model: 'text-davinci-003',
+  const response = await openAI.createChatCompletion({
+    model: 'gpt-3.5-turbo',
     temperature: 0.1,
     max_tokens: 3600,
-    prompt: `
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a blog post generator.'
+      },
+      {
+        role: 'user',
+        content: `
     Write a long and detailed SEO-friendly blog post about ${topic}, 
     that targets the following comma-separated keywords: ${keywords}.
     The content should be formatted in SEO-friendly HTML.
     The response must also include appropriate HTML title and meta description content.
-    The return format must be stringified JSON in the following format:
+    The return format must be valid stringified JSON (with no \n or \t in the output) in the following format:
     {
       "content": post content goes here,
       "title": title goes here,
       "description": meta description goes here
     }`
+      }
+    ]
   });
 
   await db.collection('users').updateOne(
@@ -62,7 +72,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
   );
 
-  const parsed = JSON.parse(response.data.choices[0]?.text.replaceAll('\n', ''));
+  const parsed = JSON.parse(response.data?.choices[0]?.message.content.replaceAll('\n', ''));
   const post = await db.collection('posts').insertOne({
     title: parsed?.title,
     content: parsed?.content,
@@ -76,5 +86,4 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ postId: post.insertedId });
 };
 
-/* @ts-ignore */
 export default withApiAuthRequired(handler);
